5fe5eb577fa6782d612a082e01c4b643f536cfc8
WentTheFox <mail@went.tf>
2015-04-11 05:16:05 +0200

    Adding Google Analytics tracking code

--- www/views/footer.php
+++ www/views/footer.php
@@ -9,6 +9,18 @@
 <script src="<?=djpth('js>'.(!preg_match('/\.js\.php$/',strtok($js,'?')) ? "$js.js" : $js))?>"></script>
 <?php 	} ?>
 <script>
+<?php if (!isset($_SERVER['HTTP_DNT'])){ ?>
+  /* We respect your privacy. Enable "Do Not Track" in your browser, and this tracking code will disappear. */
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-35773360-5', 'auto');
+  ga('require', 'displayfeatures');
+  ga('send', 'pageview');
+
+<?php } ?>
 $(function(){
 	var $nav = $('header nav');
 	if ($nav.length > 0){

3bbc91be5e63d4f5d79212d26e001d48e4e43708
WentTheFox <mail@went.tf>
2015-04-19 20:44:20 +0200

    Adding a hacky, Windows option to retrieve the latest commit info

--- setup/conf.php
+++ setup/conf.php
@@ -19,3 +19,11 @@
 	// Get latest commit version & time from Git \\
 	define('LATEST_COMMIT_ID',shell_exec("git rev-parse --short HEAD"));
 	define('LATEST_COMMIT_TIME',date('c',strtotime(shell_exec("git log -1 --date=short --pretty=format:%ci"))));
+
+	// Use For Window, without shell_exec and/or git \\
+	/* COMMENT OUT THE SECTION ABOVE AND UNCOMMENT THIS TO USE
+	$gitfpath = dirname(__FILE__).'/../.git/refs/heads/master';
+	define('LATEST_COMMIT_ID',substr(file_get_contents($gitfpath),0,7).'-local');
+	define('LATEST_COMMIT_TIME',date('c',filemtime($gitfpath)));
+	unset($gitfpath);
+	*/

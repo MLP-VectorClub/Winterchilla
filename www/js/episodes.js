6721535aa02954b7cdeb08006c72df72ccc4c195
WentTheFox <mail@went.tf>
2015-07-03 18:44:13 +0200

    Universal time format on /episodes page

--- www/js/episodes.js
+++ www/js/episodes.js
@@ -1,8 +1,14 @@
 $(function(){
-	var $eptableBody = $('#episodes').children('tbody');
-	$eptableBody.on('updatetimes',function(){
-		$eptableBody.children().children(':last-child').children('time.nodt').each(function(){
-			this.innerHTML = new Date($(this).attr('datetime')).toLocaleString();
-		});
-	}).trigger('updatetimes');
+	function pad(n){return n<10?'0'+n:n}
+	var $eptableBody = $('#episodes').children('tbody'),
+		monthNames = 'January|February|March|April|May|June|July|August|September|October|November|December'.split('|');
+	$eptableBody.children().children(':last-child').children('time.nodt').each(function(){
+		var d = new Date($(this).attr('datetime')),
+			str =
+				[pad(d.getDate()), monthNames[d.getMonth()], d.getFullYear()].join('-')+' '+
+				[pad(d.getHours()), pad(d.getMinutes())].join(':'),
+			secs = d.getSeconds();
+		if (secs > 0) str += ':'+pad(secs);
+		this.innerHTML = str;
+	});
 });

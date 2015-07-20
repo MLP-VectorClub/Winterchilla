27ece5252c291b0715f10439c4ed8131da3189cd
WentTheFox <mail@went.tf>
2015-07-20 10:04:25 +0200

    Fix #1

--- www/do.php
+++ www/do.php
@@ -372,7 +372,7 @@
 								};
 								if (!isset($vid->provider) || $vid->provider['name'] !== $k)
 									respond("Incorrect {$VIDEO_PROVIDER_NAMES[$k]} URL specified");
-								$set = $vid->$id;
+								$set = $vid->id;
 							}
 
 							$video = $Database->whereEp($Episode)->where('provider', $k)->getOne('episodes__videos','COUNT(*) as count');

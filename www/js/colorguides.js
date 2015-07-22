8aef31fc0245220de17aa563daddc94e558a919c
WentTheFox <mail@went.tf>
2015-07-22 17:52:04 +0200

    Changing to an even better copying solution

--- www/js/colorguides.js
+++ www/js/colorguides.js
@@ -13,16 +13,50 @@ $(function(){
 				style: { classes: 'qtip-tag qtip-tag-'+this.className }
 			});
 		});
-		var $colors = $('ul.colors').find('li > span[title][title!=""]').on('click',function(e){
-			e.preventDefault();
-			prompt('Ctrl+C to copy, Enter to close', $(this).text().trim())
-		}).qtip({
-			content: {
-				text: 'Click to copy HEX color code to clipboard',
-				title: function(){ return $(this).attr('title') }
-			},
-			position: { my: 'bottom center', at: 'top center', viewport: true },
-			style: { classes: 'qtip-see-thru' }
+		$('ul.colors').find('li > span[title][title!=""]').each(function(){
+			var $this = $(this);
+			$this.on('click',function(e){
+				e.preventDefault();
+				if (!document.queryCommandSupported('copy')){
+					$this.qtip('option', 'content.title', false);
+					$this.qtip('option', 'content.text', $this.attr('oldtitle'));
+					return $this.off('click') || true;
+				}
+
+				var $helper = $(document.createElement('textarea')),
+					success = false;
+				$helper
+					.css({
+						opacity: 0,
+						visibility: 'hidden',
+						width: 0,
+						height: 0,
+						position: 'absolute',
+						left: '-10px',
+						top: '-10px',
+						display: 'block',
+					})
+					.text($this.text().trim())
+					.appendTo('body');
+				$helper.get(0).focus();
+				$helper.get(0).select();
+
+				try {
+					success = document.execCommand('copy');
+				} catch(e){}
+
+				if (!success)
+					$.Dialog.fail('Copy to clipboard', 'Copying text to clipboard failed!');
+				$helper.remove();
+			});
+			$this.qtip({
+				content: {
+					text: 'Click to copy HEX color code to clipboard',
+					title: function(){ return $(this).attr('title') }
+				},
+				position: { my: 'bottom center', at: 'top center', viewport: true },
+				style: { classes: 'qtip-see-thru' }
+			});
 		});
 	}
 	tooltips();

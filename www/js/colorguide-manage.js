5f13c1d4aaff63d19e5ef29411f1d7ac9737dc5d
WentTheFox <mail@went.tf>
2015-07-23 17:42:04 +0200

    Fix click-to-copy feature on /colorguide

--- www/js/colorguide-manage.js
+++ www/js/colorguide-manage.js
@@ -15,60 +15,57 @@ $(function(){
 		}});
 	});
 
-	$('ul.colors').children('li').each(function(){
-		$(this).ctxmenu(
-			[
-				{text: "Edit "+color+" group", icon: 'pencil', click: function(){
-					$.Dialog.info('Edit '+color+' group triggered', 'yay');
-				}},
-				{text: "Add new group", icon: 'folder-add', click: function(){
-					$.Dialog.info('Add new group triggered', 'yay');
-				}},
-				{text: "Add new "+color, icon: 'plus', click: function(){
-					$.Dialog.info('Add new color triggered', 'yay');
-				}}
-			],
-			function($el){ return Color+' group '+$el.children().first().text().trim().replace(':','') }
-		);
-	}).children('span[title][title!=""]').each(function(){
-		var $this = $(this);
-		$this.on('click',function(e){
-			e.preventDefault();
-			if (!document.queryCommandSupported('copy')){
-				prompt('Copy with Ctrl+C, close with Enter');
-				return true;
-			}
+	$('ul.colors').children('li').ctxmenu(
+		[
+			{text: "Edit "+color+" group", icon: 'pencil', click: function(){
+				$.Dialog.info('Edit '+color+' group triggered', 'yay');
+			}},
+			{text: "Add new group", icon: 'folder-add', click: function(){
+				$.Dialog.info('Add new group triggered', 'yay');
+			}},
+			{text: "Add new "+color, icon: 'plus', click: function(){
+				$.Dialog.info('Add new color triggered', 'yay');
+			}}
+		],
+		function($el){ return Color+' group '+$el.children().first().text().trim().replace(':','') }
+	).children('span').off('click').on('click',function(e){
+		e.preventDefault();
+		if (!document.queryCommandSupported('copy')){
+			prompt('Copy with Ctrl+C, close with Enter');
+			return true;
+		}
 
-			var $helper = $(document.createElement('textarea')),
-				success = false;
-			$helper
-				.css({
-					opacity: 0,
-					visibility: 'hidden',
-					width: 0,
-					height: 0,
-					position: 'absolute',
-					left: '-10px',
-					top: '-10px',
-					display: 'block',
-				})
-				.text($this.text().trim())
-				.appendTo('body');
-			$helper.get(0).focus();
-			$helper.get(0).select();
+		var $helper = $(document.createElement('textarea')),
+			success = false;
+		$helper
+			.css({
+				opacity: 0,
+				width: 0,
+				height: 0,
+				position: 'absolute',
+				left: '-10px',
+				top: '-10px',
+				display: 'block',
+			})
+			.text(this.innerHTML.trim())
+			.appendTo('body')
+			.focus();
+		$helper.get(0).select();
 
-			try {
-				success = document.execCommand('copy');
-			} catch(e){}
+		try {
+			success = document.execCommand('copy');
+		} catch(e){}
 
-			if (!success)
-				$.Dialog.fail('Copy to clipboard', 'Copying text to clipboard failed!');
+		if (!success)
+			$.Dialog.fail('Copy to clipboard', 'Copying text to clipboard failed!');
+		setTimeout(function(){
 			$helper.remove();
-		});
+		}, 1);
 	}).ctxmenu(
 		[
-			{text: "Copy "+color, icon: 'pencil', click: function(){
-				$.Dialog.info('Edit '+color+' triggered', 'yay');
+			{text: "Copy "+color, icon: 'clipboard', 'default': true, click: function(){
+				console.log($(this));
+				$(this).triggerHandler('click');
 			}},
 			{text: "Edit "+color, icon: 'pencil', click: function(){
 				$.Dialog.info('Edit '+color+' triggered', 'yay');

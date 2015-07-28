19d26eae8d795cd37d3624fb61824a458d18585b
WentTheFox <mail@went.tf>
2015-07-28 23:41:40 +0200

    Add $git variable in config for Windows-based git without PATH

--- setup/conf.php
+++ setup/conf.php
@@ -12,7 +12,16 @@
 	// Google Analytics Tracking Code \\
 	define('GA_TRACKING_CODE','');
 
-	// Get latest commit version & time from Git \\
-	define('LATEST_COMMIT_ID',rtrim(shell_exec("git rev-parse --short=4 HEAD")));
-	define('IS_LATEST_COMMIT',strpos(shell_exec("git log -1 --oneline"), LATEST_COMMIT_ID) === 0);
-	define('LATEST_COMMIT_TIME',date('c',strtotime(shell_exec("git log -1 --date=short --pretty=format:%ci"))));
+	/**
+	 * Get latest commit version & time from Git
+	 * -----------------------------------------
+	 * Windows (without using PATH):
+	 *   $git = '<drive>:\path\to\git.exe';
+	 *
+	 * Linux/Unix or Windows (using PATH):
+	 *   $git = 'git';
+	 */
+	$git = 'git';
+	define('LATEST_COMMIT_ID',rtrim(shell_exec("$git rev-parse --short=4 HEAD")));
+	define('IS_LATEST_COMMIT',strpos(shell_exec("$git log -1 --oneline"), LATEST_COMMIT_ID) === 0);
+	define('LATEST_COMMIT_TIME',date('c',strtotime(shell_exec("$git log -1 --date=short --pretty=format:%ci"))));

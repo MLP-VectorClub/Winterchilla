49d46fc2d7b55ea2359def07ae4c19a73e27e505
WentTheFox <mail@went.tf>
2015-09-24 21:57:48 +0200

    Make sure fullsize is not empty

--- www/includes/Utils.php
+++ www/includes/Utils.php
@@ -1225,7 +1225,7 @@ HTML;
 				else $Image = "<div class='image deviation error'><a href='http://fav.me/{$R['deviation_id']}'>Preview unavailable<br><small>Click to view</small></a></div>";
 				if (PERM('inspector')){
 					$Image .= $label;
-					if (isset($R['fullsize']))
+					if (!empty($R['fullsize']))
 						$Image .= "<a href='{$R['fullsize']}' class='original' target='_blank'>Direct link to original image</a>";
 				}
 			}

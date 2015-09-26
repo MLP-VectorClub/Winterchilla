50fe8c19941b0668c4aaa8d955df90819b891e91
WentTheFox <mail@went.tf>
2015-09-26 15:26:13 +0200

    Remove IP reversal check from /browser

--- www/views/browser.php
+++ www/views/browser.php
@@ -4,5 +4,4 @@
     <p>on <?=$browser['platform']?></p>
 
     <?=Notice('info','Browser recognition testing page',"The following page is used to make sure that the site's browser detection script works as it should. If you're seeing a browser and/or operating system that's different from what you're currently using, please let us know.")?>
-    <?=Notice('info','CloudFlare IP Reversal check',"Your IP address appears to be {$_SERVER['REMOTE_ADDR']}".(PERM('developer')?"\n\n<pre>".var_export($_SERVER, true).'</pre>':''))?>
 </div>

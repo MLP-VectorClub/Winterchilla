c9ab2825cc4372be3563e6b92684fce0bfc3a547
WentTheFox <mail@went.tf>
2015-10-25 22:14:29 +0100

    Change wording of universal colors description

--- www/views/colorguide.php
+++ www/views/colorguide.php
@@ -10,7 +10,7 @@
 	<div id='universal'>
 		<div>
 			<strong>Universal colors</strong>
-			<div class="notes">These colors apply to most characters in the show. Unless a different color is specified, use these.</div>
+			<div class="notes">These colors apply to most characters in the show. Use these unless a different color is specified.</div>
 			<ul class="colors static">
 				<li>
 					<span class='cat'>Normal: </span><span style='background-color:#FFFFFF' title='Teeth Fill'>#FFFFFF</span><span style='background-color:#B0D8E7' title='Teeth Outline'>#B0D8E7</span><span style='background-color:#BD1C77' title='Mouth Fill'>#BD1C77</span><span style='background-color:#A41967' title='Darker Mouth Fill'>#A41967</span><span style='background-color:#841555' title='Darkest Mouth Fill'>#841555</span><span style='background-color:#F17031' title='Tongue'>#F17031</span><span style='background-color:#BE4406' title='Tongue Dark'>#BE4406</span><span style='background-color:#000000' title='Emotional Turmoil (up to 15% opacity)'>#000000</span>

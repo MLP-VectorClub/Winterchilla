d23a96d9ca30ff2cf8b4a754cbe176f3f72969e9
WentTheFox <mail@went.tf>
2016-01-31 04:04:44 +0100

    Change Google Analytics script inclusion method

--- www/views/footer.php
+++ www/views/footer.php
@@ -8,20 +8,20 @@
 var REWRITE_REGEX = <?=str_replace('~','/',str_replace('/','\/',REWRITE_REGEX))?>i,
 	SITE_TITLE = '<?=SITE_TITLE?>',
 	PRINTABLE_ASCII_REGEX = '<?=PRINTABLE_ASCII_REGEX?>',
-	DocReady = [];<?
+	DocReady = [];</script><?
 	if (!isset($_SERVER['HTTP_DNT']) && !empty(GA_TRACKING_CODE) && !PERM('inspector')){ ?>
 
-/* We respect your privacy. Enable "Do Not Track" in your browser, and this tracking code will disappear. */
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-ga('create', '<?=GA_TRACKING_CODE?>', 'auto');
+<!-- We respect your privacy. Enable "Do Not Track" in your browser, and this tracking code will disappear. -->
+<script async src='https://www.google-analytics.com/analytics.js'></script>
+<script>
+// http://nickcraver.com/blog/2015/03/24/optimization-considerations/
+var ga=ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+ga('create','<?=GA_TRACKING_CODE?>','auto');
 ga('require', 'displayfeatures');
-ga('send', 'pageview');
-<?  } ?></script>
-<?  if (isset($customJS)) foreach ($customJS as $js){
+ga('send','pageview');
+</script>
+<?  }
+	if (isset($customJS)) foreach ($customJS as $js){
 		echo "<script src='$js'></script>\n";
 	} ?>
 </body>

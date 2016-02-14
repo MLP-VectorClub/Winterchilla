d3cdf52c053150cc1ae4f0ed7ebf96114a17ab4e
WentTheFox <mail@went.tf>
2016-02-14 12:37:58 +0100

    Revert to old tracking code

--- www/views/footer.php
+++ www/views/footer.php
@@ -12,10 +12,11 @@ var REWRITE_REGEX = <?=str_replace('~','/',str_replace('/','\/',REWRITE_REGEX))?
 	if (!isset($_SERVER['HTTP_DNT']) && !empty(GA_TRACKING_CODE) && !PERM('inspector')){ ?>
 
 <!-- We respect your privacy. Enable "Do Not Track" in your browser, and this tracking code will disappear. -->
-<script async src='https://www.google-analytics.com/analytics.js'></script>
 <script>
-/* http://nickcraver.com/blog/2015/03/24/optimization-considerations/ */
-var ga=ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 ga('create','<?=GA_TRACKING_CODE?>','auto');
 ga('require', 'displayfeatures');
 ga('send','pageview');

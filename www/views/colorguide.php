5a2734eac59ba00f6e518d17ccb5ac3f51ed165d
WentTheFox <mail@went.tf>
2016-03-27 17:07:14 +0200

    Fix misspelling of guides under guide page heading

--- www/views/colorguide.php
+++ www/views/colorguide.php
@@ -1,7 +1,7 @@
 <div id="content">
 	<h1><?=$heading?></h1>
 	<p>A searchable list of character <?=$color?>s from the <?=$EQG?'movies':'show'?></p>
-	<p class="align-center">If you can't find a character here, check the old gudes: <a href="https://sta.sh/0kic0ngp3fy">Pony</a> / <a href="http://fav.me/d7120l1">EQG</a></p>
+	<p class="align-center">If you can't find a character here, check the old guides: <a href="https://sta.sh/0kic0ngp3fy">Pony</a> / <a href="http://fav.me/d7120l1">EQG</a></p>
 <? if (PERM('inspector')){ ?>
 	<div class="notice warn tagediting">
 		<label>Limited editing</label>

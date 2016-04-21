20a46c265a2fc2b90db7610b0ec2071ed36cad3d
WentTheFox <mail@went.tf>
2016-04-21 12:46:39 +0200

    Revert "Maintenance notice"
    This reverts commit 3af45269e42895eb5da586d18fabcf14c6506d38.

--- www/views/dberr.php
+++ www/views/dberr.php
@@ -12,11 +12,7 @@
 <div id="content">
 	<h1>Database connection error</h1>
 	<p>Could not connect to database on <?=DB_HOST?></p>
-	<?php
-	/*
-	<?=Notice('info','<span class="typcn typcn-info-large"></span> The database of our website cannot be reached. Hopefully this is just a temporary issue and everything will be back to normal soon. Sorry for the inconvenience.',true)?>	 */
-		echo Notice('info','<span class="typcn typcn-info-large"></span> <strong>The website is currently undergoing maintenance, sorry for the inconvenience caused! We\'ll be back shortly. ~ DJDavid98 <time datetime="2016-04-21T09:22:00Z"></time></strong>',true)
-	?>
+	<?=Notice('info','<span class="typcn typcn-info-large"></span> The database of our website cannot be reached. Hopefully this is just a temporary issue and everything will be back to normal soon. Sorry for the inconvenience.',true)?>
 	<?php
 	$code = $e->getCode();
 	$CODE_ERRORS = array(

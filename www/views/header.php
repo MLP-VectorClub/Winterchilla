56b67ccf48ab88d9fe99f0049c2cb2b99042d2a2
WentTheFox <mail@went.tf>
2016-04-26 21:19:21 +0200

    Add all kinds of favicons just to be sure

--- www/views/header.php
+++ www/views/header.php
@@ -28,6 +28,27 @@
 	<meta name="format-detection" content="telephone=no">
 	<meta name="theme-color" content="#2C73B1">
 	<link rel="image_src" href="<?=$ThumbImage?>">
+	
+	<link rel="apple-touch-icon" sizes="57x57" href="/img/favicons/apple-touch-icon-57x57.png?v=1">
+	<link rel="apple-touch-icon" sizes="60x60" href="/img/favicons/apple-touch-icon-60x60.png?v=1">
+	<link rel="apple-touch-icon" sizes="72x72" href="/img/favicons/apple-touch-icon-72x72.png?v=1">
+	<link rel="apple-touch-icon" sizes="76x76" href="/img/favicons/apple-touch-icon-76x76.png?v=1">
+	<link rel="apple-touch-icon" sizes="114x114" href="/img/favicons/apple-touch-icon-114x114.png?v=1">
+	<link rel="apple-touch-icon" sizes="120x120" href="/img/favicons/apple-touch-icon-120x120.png?v=1">
+	<link rel="apple-touch-icon" sizes="144x144" href="/img/favicons/apple-touch-icon-144x144.png?v=1">
+	<link rel="apple-touch-icon" sizes="152x152" href="/img/favicons/apple-touch-icon-152x152.png?v=1">
+	<link rel="apple-touch-icon" sizes="180x180" href="/img/favicons/apple-touch-icon-180x180.png?v=1">
+	<link rel="icon" type="image/png" href="/img/favicons/favicon-32x32.png?v=1" sizes="32x32">
+	<link rel="icon" type="image/png" href="/img/favicons/android-chrome-192x192.png?v=1" sizes="192x192">
+	<link rel="icon" type="image/png" href="/img/favicons/favicon-96x96.png?v=1" sizes="96x96">
+	<link rel="icon" type="image/png" href="/img/favicons/favicon-16x16.png?v=1" sizes="16x16">
+	<link rel="manifest" href="/img/favicons/manifest.json?v=1">
+	<link rel="mask-icon" href="/img/favicons/safari-pinned-tab.svg?v=1" color="#2c73b1">
+	<meta name="apple-mobile-web-app-title" content="MLP-VectorClub">
+	<meta name="application-name" content="MLP-VectorClub">
+	<meta name="msapplication-TileColor" content="#2c73b1">
+	<meta name="msapplication-TileImage" content="/img/favicons/mstile-144x144.png?v=1">
+	<meta name="msapplication-config" content="/img/favicons/browserconfig.xml?v=1">
 
 <?php if (isset($norobots)){ ?>
 	<meta name="robots" content="noindex, nofollow">

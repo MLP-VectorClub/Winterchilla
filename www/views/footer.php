82fc38673b674fb235767bfb4305ffd70bd6c99e
WentTheFox <mail@went.tf>
2016-05-04 17:18:12 +0200

    Move Google Analytics tracking code into the header

--- www/views/footer.php
+++ www/views/footer.php
@@ -6,21 +6,8 @@
 <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
 <script>if(!window.jQuery)document.write('\x3Cscript src="/js/jquery-2.1.4.min.js">\x3C/script>');
 var PRINTABLE_ASCII_REGEX = '<?=PRINTABLE_ASCII_REGEX?>',
-	DocReady = [];</script><?
-	if (!isset($_SERVER['HTTP_DNT']) && !empty(GA_TRACKING_CODE) && !Permission::Sufficient('inspector')){ ?>
-
-<!-- We respect your privacy. Enable "Do Not Track" in your browser, and this tracking code will disappear. -->
-<script>
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-ga('create','<?=GA_TRACKING_CODE?>','auto');
-ga('require', 'displayfeatures');
-ga('send','pageview');
-</script>
-<?  }
-	if (isset($customJS)) foreach ($customJS as $js){
+	DocReady = [];</script>
+<?  if (isset($customJS)) foreach ($customJS as $js){
 		echo "<script src='$js'></script>\n";
 	} ?>
 </body>
--- www/views/header.php
+++ www/views/header.php
@@ -57,6 +57,17 @@
 <?php if (isset($customCSS)) foreach ($customCSS as $css){ ?>
 	<link rel="stylesheet" href="<?=$css?>">
 <?php } ?>
+<?  if (!empty(GA_TRACKING_CODE) && Permission::Insufficient('developer')){ ?>
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create','<?=GA_TRACKING_CODE?>','auto');
+ga('require', 'displayfeatures');
+ga('send','pageview');
+</script>
+<?  } ?>
 </head>
 <body class="loading">
 

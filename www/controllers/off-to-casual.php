38287bb43d7dbf7160aa3f0ec37c05005f0c9223
WentTheFox <mail@went.tf>
2016-05-26 23:17:57 +0200

    Add another /off-to-casual image

--- www/controllers/off-to-casual.php
+++ www/controllers/off-to-casual.php
@@ -4,6 +4,7 @@
 		'http://i.imgur.com/C7T0npq.png', // Original by DJDavid98
 		'http://i.imgur.com/RwnT8EX.png', // Coco & Rarity by Pirill
 		'http://i.imgur.com/qg9Y1LN.png', // Applebloom's new CM by Drakizora
+		'http://i.imgur.com/cxCzsB8.png', // Applebloom falling by Drakizora
 	);
 
 	$k = isset($_GET['i']) && is_numeric($_GET['i']) ? max(0,min(count($possible_images)-1,intval($_GET['i'], 10))) : array_rand($possible_images);

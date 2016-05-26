5de5198aebfec6264856376cf7145b72946b474a
WentTheFox <mail@went.tf>
2016-05-26 23:00:20 +0200

    Fix /off-to-casual image URL array indexing

--- www/controllers/off-to-casual.php
+++ www/controllers/off-to-casual.php
@@ -1,8 +1,8 @@
 <?php
 
 	$possible_images = array(
-		'http://i.imgur.com/RwnT8EX.png',
-		'http://i.imgur.com/C7T0npq.png',
+		'http://i.imgur.com/C7T0npq.png', // Original
+		'http://i.imgur.com/RwnT8EX.png', // Coco & Rarity
 	);
 
 	$k = isset($_GET['i']) && is_numeric($_GET['i']) ? max(0,min(count($possible_images)-1,intval($_GET['i'], 10))) : array_rand($possible_images);

ebcd92e13d87ca749b5eed6a640df3b31fae51cd
WentTheFox <mail@went.tf>
2016-06-16 22:57:13 +0200

    Remove manual New Relic tracking script insertion

--- www/views/footer.php
+++ www/views/footer.php
@@ -4,7 +4,6 @@
 	<footer><?=CoreUtils::GetFooter()?></footer>
 
 <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
-<script src="/js/newrelic.browser.min.js"></script>
 <script>if(!window.jQuery)document.write('\x3Cscript src="/js/jquery-2.1.4.min.js">\x3C/script>');
 var PRINTABLE_ASCII_PATTERN = '<?=PRINTABLE_ASCII_PATTERN?>',
 	DocReady = [];</script>

da18c07dcbba0a0a57e8e0065106dde43426298c
WentTheFox <mail@went.tf>
2016-08-06 22:47:06 +0200

    Redo /off-to-casual "randomness" / specific image linking

--- www/controllers/off-to-casual.php
+++ www/controllers/off-to-casual.php
@@ -7,6 +7,14 @@
 		'http://i.imgur.com/cxCzsB8.png', // Applebloom falling by Drakizora
 		'http://i.imgur.com/iUZe3O2.png', // CMs floating around Applebloom by Drakizora
 	);
+	$image_count = count($possible_images);
 
-	$k = isset($_GET['i']) && is_numeric($_GET['i']) ? max(0,min(count($possible_images)-1,intval($_GET['i'], 10))) : array_rand($possible_images);
+	$data = regex_replace(new RegExp('^(\d+)\.(png|jpe?g|gif)$'), '$1', $data);
+
+	if (is_numeric($data))
+		$k = max(0,min($image_count-1,intval($data, 10)-1));
+	else {
+		$k = intval(date('i'), 10) % $image_count;
+	}
+
 	HTTP::Redirect($possible_images[$k], true, 302);

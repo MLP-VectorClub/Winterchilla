703046a60090a3268b8b12a786f290b227af3ce7
WentTheFox <mail@went.tf>
2017-04-23 23:02:34 +0200

    Apple devices just can't stop with the 404 requests

--- includes/views/_layout.php
+++ includes/views/_layout.php
@@ -93,15 +93,15 @@ $Title = CoreUtils::escapeHTML($Title); ?>
 	<meta name="theme-color" content="#2C73B1">
 	<link rel="image_src" href="<?=$ThumbImage?>">
 
-	<link rel="apple-touch-icon" sizes="57x57" href="/img/favicons-v1/apple-touch-icon-57x57.png">
-	<link rel="apple-touch-icon" sizes="60x60" href="/img/favicons-v1/apple-touch-icon-60x60.png">
-	<link rel="apple-touch-icon" sizes="72x72" href="/img/favicons-v1/apple-touch-icon-72x72.png">
-	<link rel="apple-touch-icon" sizes="76x76" href="/img/favicons-v1/apple-touch-icon-76x76.png">
-	<link rel="apple-touch-icon" sizes="114x114" href="/img/favicons-v1/apple-touch-icon-114x114.png">
-	<link rel="apple-touch-icon" sizes="120x120" href="/img/favicons-v1/apple-touch-icon-120x120.png">
-	<link rel="apple-touch-icon" sizes="144x144" href="/img/favicons-v1/apple-touch-icon-144x144.png">
-	<link rel="apple-touch-icon" sizes="152x152" href="/img/favicons-v1/apple-touch-icon-152x152.png">
-	<link rel="apple-touch-icon" sizes="180x180" href="/img/favicons-v1/apple-touch-icon-180x180.png">
+	<link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png">
 	<link rel="icon" type="image/png" href="/img/favicons-v1/favicon-32x32.png" sizes="32x32">
 	<link rel="icon" type="image/png" href="/img/favicons-v1/android-chrome-192x192.png" sizes="192x192">
 	<link rel="icon" type="image/png" href="/img/favicons-v1/favicon-96x96.png" sizes="96x96">

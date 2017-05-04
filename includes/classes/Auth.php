5a2be8742e60b93e57d422295a0342ae870cf3af
WentTheFox <mail@went.tf>
2017-05-04 18:47:25 +0200

    Document Auth class

--- includes/classes/Auth.php
+++ includes/classes/Auth.php
@@ -5,13 +5,17 @@ namespace App;
 use App\Models\Session;
 use App\Models\User;
 
+/**
+ * This class provides global access to some site-wide variables.
+ * It's much more straight-forward and IDE friendly than the old approach using `global $var`
+ */
 class Auth {
-	/** @var User|null */
+	/** @var User|null Currently authenticated user (or null if guest) */
 	static $user = null;
 
-	/** @var Session|null */
+	/** @var Session|null Current session (or null if guest) */
 	static $session = null;
 
-	/** @var bool */
+	/** @var bool True if signed in, false if guest */
 	static $signed_in = false;
 }

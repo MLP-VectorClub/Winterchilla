d18a501a2dc8866d9963faf5c14c59a7afc551ef
WentTheFox <mail@went.tf>
2017-07-22 13:03:16 +0200

    Add post-receive hook to repository

---
+++ setup/post-receive.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+echo "##### post-receive hook #####"
+PROJ_DIR="/path/to/proj_dir"
+if [ ! -d "$PROJ_DIR" ]; then
+    echo "The directory $PROJ_DIR does not exist"
+    exit 1
+fi
+while read oldrev newrev refname
+do
+    branch=$(git rev-parse --symbolic --abbrev-ref ${refname})
+    if [ "master" == "$branch" ]; then
+		GIT_DIR="$PROJ_DIR/.git"
+        CMD_CD="cd $PROJ_DIR"
+		CMD_FETCH="git fetch"
+		CMD_CHECKOUT="git -c advice.detachedHead=false checkout $newrev -f"
+		CMD_COMPOSER="sudo -u www-data composer install --no-dev 2>&1"
+
+		echo "$ $CMD_CD"
+		eval ${CMD_CD}
+		echo "$ $CMD_FETCH"
+		eval ${CMD_FETCH}
+		echo "$ $CMD_CHECKOUT"
+		eval ${CMD_CHECKOUT}
+		echo "$ $CMD_COMPOSER"
+		eval ${CMD_COMPOSER}
+	else
+		echo "Push to $branch ignored"
+    fi
+done
+echo "##### end post-receive hook #####"

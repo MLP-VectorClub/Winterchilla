c013c91f78570ddd45b4f9b263669f477a7baf81
WentTheFox <mail@went.tf>
2017-08-14 23:59:57 +0200

    Fix broken tests

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -3,7 +3,7 @@
 use PHPUnit\Framework\TestCase;
 
 class UserTest extends TestCase {
-	public function testGetProfileLink(){
+	public function testToAnchor(){
 		$User = new \App\Models\User([
 			'name' => 'TestUser',
 			'avatar_url' => '/img/guest.svg',
@@ -11,16 +11,23 @@ class UserTest extends TestCase {
 		$result = $User->toAnchor();
 		static::assertEquals("<a href='/@TestUser' class='da-userlink local'><span class='name'>TestUser</span></a>", $result, 'Testing default parameter return value format');
 		$result = $User->toAnchor(\App\Models\User::WITH_AVATAR);
-		static::assertEquals("<a href='/@TestUser' class='da-userlink local with-avatar'><img src='/img/guest.svg' class='avatar' alt='avatar'> <span class='name'>TestUser</span></a>", $result, 'Testing full format return value');
+		static::assertEquals("<a href='/@TestUser' class='da-userlink local with-avatar provider-deviantart'><img src='/img/guest.svg' class='avatar' alt='avatar'> <span class='name'>TestUser</span></a>", $result, 'Testing full format return value');
 	}
 
-	public function testGetDALink(){
+	public function testToDALink(){
 		$User = new \App\Models\User([
 			'name' => 'TestUser',
 			'avatar_url' => '/img/guest.svg',
 		]);
 		$result = $User->toDALink();
 		static::assertEquals('http://testuser.deviantart.com/', $result, 'Testing URL format return value');
+	}
+
+	public function testToDAAnchor(){
+		$User = new \App\Models\User([
+			'name' => 'TestUser',
+			'avatar_url' => '/img/guest.svg',
+		]);
 		$result = $User->toDAAnchor();
 		static::assertEquals("<a href='http://testuser.deviantart.com/' class='da-userlink'><span class='name'>TestUser</span></a>", $result, 'Testing default parameter return value format');
 		$result = $User->toDAAnchor(\App\Models\User::WITH_AVATAR);
@@ -33,6 +40,6 @@ class UserTest extends TestCase {
 			'avatar_url' => '/img/guest.svg',
 		]);
 		$result = $User->getAvatarWrap(' app-illustrator');
-		static::assertEquals("<div class='avatar-wrap app-illustrator'><img src='/img/guest.svg' class='avatar' alt='avatar'></div>", $result);
+		static::assertEquals("<div class='avatar-wrap provider-deviantart app-illustrator' data-for='TestUser'><img src='/img/guest.svg' class='avatar' alt='avatar'></div>", $result);
 	}
 }

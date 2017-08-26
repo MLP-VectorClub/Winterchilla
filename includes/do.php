995ed8d17e9bc216e544cdb9fca1735b071c77c1
WentTheFox <mail@went.tf>
2017-08-26 16:33:25 +0200

    Fix whitespace at the end of the URL breaking routing

--- includes/do.php
+++ includes/do.php
@@ -10,7 +10,7 @@
 	$permRedirectPattern = new RegExp('^\s*(.*?)\.php(\?.*)?$','i');
 	if (preg_match($permRedirectPattern, $_SERVER['REQUEST_URI']))
 		HTTP::redirect(preg_replace($permRedirectPattern, '$1$2', $_SERVER['REQUEST_URI']));
-	$decoded_uri = urldecode(CoreUtils::trim($_SERVER['REQUEST_URI']));
+	$decoded_uri = CoreUtils::trim(urldecode($_SERVER['REQUEST_URI']));
 	if (!preg_match($REWRITE_REGEX,strtok($decoded_uri,'?'),$matches)){
 		Users::authenticate();
 		CoreUtils::notFound();

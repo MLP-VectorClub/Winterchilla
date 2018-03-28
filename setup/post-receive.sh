959a593a5dd2c82d9dbe2c830456b89f46f8d770
WentTheFox <mail@went.tf>
2018-03-28 02:52:48 +0200

    Move post-receive hook from a random bare repo inside the project folder

--- setup/post-receive.sh
+++ setup/post-receive.sh
@@ -1,39 +1,20 @@
 #!/bin/bash
 echo "##### post-receive hook #####"
-PROJ_DIR="/path/to/proj_dir"
-if [ ! -d "$PROJ_DIR" ]; then
-    echo "The directory $PROJ_DIR does not exist"
-    exit 1
-fi
-while read oldrev newrev refname
-do
-    branch=$(git rev-parse --symbolic --abbrev-ref ${refname} 2>/dev/null)
-    if [ "master" == "$branch" ]; then
-		GIT_DIR="$PROJ_DIR/.git"
-        CMD_CD="cd $PROJ_DIR"
-		CMD_FETCH="git fetch"
-		CMD_CHECKOUT="git -c advice.detachedHead=false checkout $newrev -f"
-		CMD_COMPOSER="sudo -u www-data composer install --no-dev 2>&1"
-		CMD_MIGRATE="sudo -u www-data vendor/bin/phinx migrate"
-		CMD_YARN="sudo -u www-data yarn install"
-		CMD_REDIS_CLEAR="sudo -u www-data php -f includes/scripts/clear_redis_keys.php commit_id commit_time"
+read oldrev newrev refname
+echo "Push triggered update to revision $newrev ($refname)"
 
-		echo "$ $CMD_CD"
-		eval ${CMD_CD}
-		echo "$ $CMD_FETCH"
-		eval ${CMD_FETCH}
-		echo "$ $CMD_CHECKOUT"
-		eval ${CMD_CHECKOUT}
-		echo "$ $CMD_COMPOSER"
-		eval ${CMD_COMPOSER}
-		echo "$ $CMD_MIGRATE"
-		eval ${CMD_MIGRATE}
-		echo "$ $CMD_YARN"
-		eval ${CMD_YARN}
-		echo "$ $CMD_REDIS_CLEAR"
-		eval ${CMD_REDIS_CLEAR}
-	else
-		echo "Push ignored (refname=$refname; branch=$branch)"
-    fi
-done
+CMD_COMPOSER="sudo -u www-data composer install --no-dev 2>&1"
+CMD_MIGRATE="sudo -u www-data vendor/bin/phinx migrate"
+CMD_YARN="sudo -u www-data yarn install --production"
+CMD_REDIS_CLEAR="sudo -u www-data php -f includes/scripts/clear_redis_keys.php commit_id commit_time"
+
+echo "$ $CMD_COMPOSER"
+eval ${CMD_COMPOSER}
+echo "$ $CMD_MIGRATE"
+eval ${CMD_MIGRATE}
+echo "$ $CMD_YARN"
+eval ${CMD_YARN}
+echo "$ $CMD_REDIS_CLEAR"
+eval ${CMD_REDIS_CLEAR}
+
 echo "##### end post-receive hook #####"

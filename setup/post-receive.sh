f35087134e779ae348d06c6b4bafb9966f7c6108
WentTheFox <mail@went.tf>
2018-03-28 03:00:20 +0200

    Add a fetch in post-receive hook

--- setup/post-receive.sh
+++ setup/post-receive.sh
@@ -3,11 +3,14 @@ echo "##### post-receive hook #####"
 read oldrev newrev refname
 echo "Push triggered update to revision $newrev ($refname)"
 
+CMD_FETCH="git fetch"
 CMD_COMPOSER="sudo -u www-data composer install --no-dev 2>&1"
 CMD_MIGRATE="sudo -u www-data vendor/bin/phinx migrate"
 CMD_YARN="sudo -u www-data yarn install --production"
 CMD_REDIS_CLEAR="sudo -u www-data php -f includes/scripts/clear_redis_keys.php commit_id commit_time"
 
+echo "$ $CMD_FETCH"
+eval ${CMD_FETCH}
 echo "$ $CMD_COMPOSER"
 eval ${CMD_COMPOSER}
 echo "$ $CMD_MIGRATE"

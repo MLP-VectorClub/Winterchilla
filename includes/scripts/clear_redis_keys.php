394018ccf6733fa884cfef0d4335f635dd3b3d69
WentTheFox <mail@went.tf>
2018-03-28 03:34:32 +0200

    Add number of keys deleted as output to the redis clear script

--- includes/scripts/clear_redis_keys.php
+++ includes/scripts/clear_redis_keys.php
@@ -4,4 +4,5 @@ $_dir = rtrim(__DIR__, '\/').DIRECTORY_SEPARATOR;
 require $_dir.'../../vendor/autoload.php';
 require $_dir.'../conf.php';
 
-\App\RedisHelper::del(array_slice($argv, 1));
+$num = \App\RedisHelper::del(array_slice($argv, 1)) ?? 0;
+echo basename(__FILE__).": $num key".($num===1?'':'s')." deleted successfully\n";

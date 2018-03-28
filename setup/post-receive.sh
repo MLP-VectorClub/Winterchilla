e331fab3f08681814cca8ce67fd01888baf8f093
WentTheFox <mail@went.tf>
2018-03-28 03:03:54 +0200

    Add a cd to repo root in post-receive hook

--- setup/post-receive.sh
+++ setup/post-receive.sh
@@ -3,12 +3,15 @@ echo "##### post-receive hook #####"
 read oldrev newrev refname
 echo "Push triggered update to revision $newrev ($refname)"
 
+CMD_CD="cd $(readlink -nf "$PWD/..")"
 CMD_FETCH="git fetch"
 CMD_COMPOSER="sudo -u www-data composer install --no-dev 2>&1"
 CMD_MIGRATE="sudo -u www-data vendor/bin/phinx migrate"
 CMD_YARN="sudo -u www-data yarn install --production"
 CMD_REDIS_CLEAR="sudo -u www-data php -f includes/scripts/clear_redis_keys.php commit_id commit_time"
 
+echo "$ $CMD_CD"
+eval ${CMD_CD}
 echo "$ $CMD_FETCH"
 eval ${CMD_FETCH}
 echo "$ $CMD_COMPOSER"

e2eec521da54bea51093efff164f4c7b85b8ac8b
WentTheFox <mail@went.tf>
2018-05-31 14:16:20 +0200

    Fix EventEntryVote tests

--- tests/Models/EventEntryVoteTest.php
+++ tests/Models/EventEntryVoteTest.php
@@ -2,26 +2,27 @@
 
 namespace Models;
 
+use ActiveRecord\DateTime;
 use App\Models\EventEntry;
 use App\Models\EventEntryVote;
 use PHPUnit\Framework\TestCase;
 
 class EventEntryVoteTest extends TestCase {
 	public function testIsLockedIn(){
-		$entry = new EventEntry([ 'last_edited' => '2017-01-20T10:00:00Z' ]);
-		$entryVote = new EventEntryVote([ 'cast_at' => '2017-01-20T12:00:00Z' ]);
-		$lockedIn = $entryVote->isLockedIn($entry, strtotime('+59 minutes',strtotime($entryVote->cast_at)));
-		self::assertEquals(false, $lockedIn, 'Votes shouldn\'t be locked in after an hour if the entry isn\'t edited');
-		$lockedIn = $entryVote->isLockedIn($entry, strtotime('+1 hour', strtotime($entryVote->cast_at)));
+		$entry = new EventEntry([ 'last_edited' => new DateTime('2017-01-20T10:00:00Z') ]);
+		$entryVote = new EventEntryVote([ 'cast_at' => new DateTime('2017-01-20T12:00:00Z') ]);
+		$lockedIn = $entryVote->isLockedIn($entry, strtotime('+59 minutes',$entryVote->cast_at->getTimestamp()));
+		self::assertEquals(false, $lockedIn, "Votes shouldn't be locked in after an hour if the entry isn't edited");
+		$lockedIn = $entryVote->isLockedIn($entry, strtotime('+1 hour', $entryVote->cast_at->getTimestamp()));
 		self::assertEquals(true, $lockedIn, 'Votes should be locked in after an hour if the entry isn\'t edited');
-		$lockedIn = $entryVote->isLockedIn($entry, strtotime('+1 day', strtotime($entryVote->cast_at)));
+		$lockedIn = $entryVote->isLockedIn($entry, strtotime('+1 day', $entryVote->cast_at->getTimestamp()));
 		self::assertEquals(true, $lockedIn, 'Votes should be locked in after an hour if the entry isn\'t edited');
 
-		$entry = new EventEntry([ 'last_edited' => '2017-01-20T15:40:00Z' ]);
+		$entry = new EventEntry([ 'last_edited' => new DateTime('2017-01-20T15:40:00Z') ]);
 		// Simulates that vote was cast 20 minutes before the post was edited
-		$entryVote = new EventEntryVote([ 'cast_at' => strtotime('-20 minutes', strtotime($entry->last_edited)) ]);
+		$entryVote = new EventEntryVote([ 'cast_at' => new DateTime(date('c', strtotime('-20 minutes', strtotime($entry->last_edited)))) ]);
 		// Forcibly check if the post is locked in 10 minutes after the edit
-		$lockedIn = $entryVote->isLockedIn($entry, strtotime('+10 minutes', strtotime($entry->last_edited)));
-		self::assertEquals(false, $lockedIn, 'Votes should be changable if the entry is edited after the vote is cast');
+		$lockedIn = $entryVote->isLockedIn($entry, strtotime('+10 minutes', $entry->last_edited->getTimestamp()));
+		self::assertEquals(false, $lockedIn, 'Votes should be changeable if the entry is edited after the vote is cast');
 	}
 }

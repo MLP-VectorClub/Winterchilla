ccf4f9121ca471c2d62eb1cfbafeeca40ebf674c
WentTheFox <mail@went.tf>
2018-06-12 20:11:01 +0200

    Only run npm install in post-receive hook when necessary

--- setup/post-receive.sh
+++ setup/post-receive.sh
@@ -3,8 +3,9 @@ echo "##### post-receive hook #####"
 read oldrev newrev refname
 echo "Push triggered update to revision $newrev ($refname)"
 
+GIT="env -i git"
 CMD_CD="cd $(readlink -nf "$PWD/..")"
-CMD_FETCH="env -i git fetch"
+CMD_FETCH="$GIT fetch"
 CMD_COMPOSER="sudo -u www-data composer install --no-dev 2>&1 && sudo chmod -R ug+rw vendor/"
 CMD_MIGRATE="sudo -u www-data vendor/bin/phinx migrate"
 CMD_NPM="sudo -u www-data npm install --production --no-save"
@@ -18,8 +19,14 @@ echo "$ $CMD_COMPOSER"
 eval ${CMD_COMPOSER}
 echo "$ $CMD_MIGRATE"
 eval ${CMD_MIGRATE}
-echo "$ $CMD_NPM"
-eval ${CMD_NPM}
+
+if $GIT diff --name-only $oldrev $newrev | grep "^package-lock.json"; then
+	echo "$ $CMD_NPM"
+	eval $CMD_NPM
+else
+	echo "# Skipping npm install, lockfile not modified"
+fi
+
 echo "$ $CMD_REDIS_CLEAR"
 eval ${CMD_REDIS_CLEAR}
 

b49d10375ee36fba6f55d8d5efd17c33b1b89562
WentTheFox <mail@went.tf>
2018-09-09 00:54:41 +0200

    Add missing update calls to AppearanceLastClearedNull

--- db/migrations/20171212220821_appearance_last_cleared_null.php
+++ db/migrations/20171212220821_appearance_last_cleared_null.php
@@ -5,10 +5,12 @@ use Phinx\Migration\AbstractMigration;
 class AppearanceLastClearedNull extends AbstractMigration {
 	public function up() {
 		$this->table('appearances')
-			->changeColumn('last_cleared', 'timestamp', [ 'timezone' => true, 'null' => true, 'default' => null ]);
+			->changeColumn('last_cleared', 'timestamp', [ 'timezone' => true, 'null' => true, 'default' => null ])
+			->update();
 	}
 	public function down() {
 		$this->table('appearances')
-			->changeColumn('last_cleared', 'timestamp', [ 'timezone' => true, 'null' => true, 'default' => 'CURRENT_TIMESTAMP' ]);
+			->changeColumn('last_cleared', 'timestamp', [ 'timezone' => true, 'null' => true, 'default' => 'CURRENT_TIMESTAMP' ])
+			->update();
 	}
 }

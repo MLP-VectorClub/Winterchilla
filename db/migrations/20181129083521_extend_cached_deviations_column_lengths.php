d74e0417b86a882293e3706a66ff0460fa51be4c
WentTheFox <mail@went.tf>
2018-11-29 09:42:44 +0100

    Increase cached deviations' image column lengths

--- db/migrations/20181129083521_extend_cached_deviations_column_lengths.php
+++ db/migrations/20181129083521_extend_cached_deviations_column_lengths.php
@@ -5,8 +5,8 @@ use Phinx\Migration\AbstractMigration;
 class ExtendCachedDeviationsColumnLengths extends AbstractMigration {
 	public function change() {
 		$this->table('cached_deviations')
-			->changeColumn('preview','string', ['length' => 300])
-			->changeColumn('fullsize','string', ['length' => 300])
+			->changeColumn('preview','string', ['length' => 300, 'null' => true])
+			->changeColumn('fullsize','string', ['length' => 300, 'null' => true])
 			->update();
 	}
 }

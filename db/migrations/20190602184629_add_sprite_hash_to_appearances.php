594d1d6f9d509bc0bea3b665d7ff7f67f178f7f3
WentTheFox <mail@went.tf>
2019-06-02 13:59:52 -0700

    Remove debug code from migration

--- db/migrations/20190602184629_add_sprite_hash_to_appearances.php
+++ db/migrations/20190602184629_add_sprite_hash_to_appearances.php
@@ -9,13 +9,11 @@ class AddSpriteHashToAppearances extends AbstractMigration {
 			->update();
 
 		foreach (new DirectoryIterator(SPRITE_PATH) as $file_info){
-			echo "$file_info\n";
 			if ($file_info->isDot()) continue;
 
 			$filename = $file_info->getFilename();
 			$appearance_id = explode('.', $filename)[0];
 			$hash = md5_file($file_info->getPathname());
-			echo "$appearance_id:$hash\n";
 			if ($hash !== false) {
 				$this->query("UPDATE appearances SET sprite_hash = '$hash' WHERE id = $appearance_id");
 			}

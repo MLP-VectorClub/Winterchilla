7e645e3e706118ea7df94ca76e06824a12f3c278
WentTheFox <mail@went.tf>
2019-08-18 05:59:06 +0200

    Add WIP banner above API docs

--- public/docs/index.php
+++ public/docs/index.php
@@ -10,12 +10,28 @@ CoreUtils::generateApiSchema(CoreUtils::env('PRODUCTION'));
 <head>
   <meta charset="UTF-8">
   <title>MLP Vector Club OpenAPI Documentation</title>
-  <link rel="stylesheet" type="text/css" href="https://unpkg.com/swagger-ui-dist@3/swagger-ui.css">
+  <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@3/swagger-ui.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Arimo:400,700,400italic&display=swap">
   <meta name="robots" content="noindex">
   <meta name="viewport" content="width=device-width,height=device-height,initial-scale=1,maximum-scale=2">
+  <style>
+    .notice {
+      font-family: "Arimo", sans-serif;
+      display: block;
+      margin: 0;
+      border: 2px solid;
+      border-radius: 7px;
+      color: #c00;
+      background-color: #fdd;
+      padding: 10px;
+      font-size: 16px;
+      font-weight: bold;
+    }
+  </style>
 </head>
 
 <body>
+<div class="notice">This API is still in development, everything is subject to change. Use with caution.</div>
 <div id="swagger-ui"></div>
 <script src="https://unpkg.com/swagger-ui-dist@3/swagger-ui-bundle.js"></script>
 <script type="text/javascript">

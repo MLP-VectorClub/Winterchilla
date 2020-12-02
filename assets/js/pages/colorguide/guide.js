179828abdd87fe0ccfa41e0af5c0b9921a82bf80
WentTheFox <mail@went.tf>
2020-12-02 12:51:54 +0100

    Update Ai script running shortcut for MacOS

--- assets/js/pages/colorguide/guide.js
+++ assets/js/pages/colorguide/guide.js
@@ -90,7 +90,7 @@
           : $content.children('h1')
       ).text().trim(),
       pressAi = navigator && navigator.userAgent && /Macintosh/i.test(navigator.userAgent)
-        ? '<kbd>\u2318</kbd><kbd>F12</kbd>'
+        ? '<kbd>fn</kbd><kbd>\u2318</kbd><kbd>F12</kbd> for Big Sur, previous versions use <kbd>\u2318</kbd><kbd>F12</kbd>'
         : '<kbd>Ctrl</kbd><kbd>F12</kbd>',
       $instr = $.mk('div').html(
         `<div class='hidden section ai'>
@@ -99,7 +99,7 @@
 						<li>Because Illustrator uses a proprietary format for swatch files, you must download a script <a href='/dist/Import%20Swatches%20from%20JSON.jsx?v=1.5' download='Import Swatches from JSON.jsx' class='btn typcn typcn-download'>by clicking here</a> to be able to import them from our site. Be sure to save it to an easy to find location because you'll need to browser for it every time you want to import colors.</li>
 						<li>Once you have the script, <a href="/cg/v/${appearanceID}s.json${token}" class="btn blue typcn typcn-download">click here</a> to download the <code>.json</code> file that you'll need to use for the import.</li>
 						<li>Now that you have the 2 files, open Illustrator, create/open a document, then go to <strong>File &rsaquo; Scripts &rsaquo; Other Script</strong> (or press ${pressAi}) then find the file with the <code>.jsx</code> extension (the one you first downloaded). A dialog will appear telling you what to do next.</li>
-						<li>Optional: If you'd like to make using the script faster, you can place it in the <code>&hellip;\\Adobe<wbr>\\Adobe Illustrator *<wbr>\\Presets<wbr>\\*<wbr>\\Scripts</code> folder, and after an Illustrator restart it'll be available as one of the options in the File &rsaquo; Scripts submenu.</li>
+						<li>Optional: If you'd like to make using the script faster, you can place it in the <code>&hellip;\\Adobe<wbr>\\Adobe Illustrator *<wbr>\\Presets<wbr>\\*<wbr>\\Scripts</code> folder, and after an Illustrator restart it'll be available as one of the options in the <strong>File &rsaquo; Scripts</strong> submenu.</li>
 					</ul>
 					<div class="responsive-embed">
 						<iframe src="https://www.youtube.com/embed/oobQZ2xiDB8" allowfullscreen async defer></iframe>

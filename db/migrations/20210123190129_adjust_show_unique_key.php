c8e9c77e7708e3f5bb8aa432c0682a5fa80a2476
WentTheFox <mail@went.tf>
2021-01-23 20:25:17 +0100

    Fix broken migration chain

--- db/migrations/20210123190129_adjust_show_unique_key.php
+++ db/migrations/20210123190129_adjust_show_unique_key.php
@@ -4,13 +4,14 @@ use Phinx\Migration\AbstractMigration;
 
 class AdjustShowUniqueKey extends AbstractMigration {
   public function up() {
+    $this->query('ALTER TABLE show DROP CONSTRAINT IF EXISTS "show_season_episode"');
     $this->table('show')
-      ->removeIndex(['season', 'episode'])
       ->addIndex(['season', 'episode', 'generation'], ['unique' => true])
       ->update();
   }
 
   public function down() {
+    $this->query('ALTER TABLE show DROP CONSTRAINT IF EXISTS "show_season_episode_generation"');
     $this->table('show')
       ->removeIndex(['season', 'episode', 'generation'])
       ->addIndex(['season', 'episode'], ['unique' => true])

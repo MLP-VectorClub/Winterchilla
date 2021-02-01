1292c8f2d6df09497f0cd93a757dc327c019fd7d
WentTheFox <mail@went.tf>
2021-02-01 20:16:18 +0100

    Update UA string in legitimateRequest method

--- app/HTTP.php
+++ app/HTTP.php
@@ -30,7 +30,7 @@ class HTTP {
       CURLOPT_BINARYTRANSFER => true,
       CURLOPT_FOLLOWLOCATION => true,
       CURLOPT_RETURNTRANSFER => true,
-      CURLOPT_USERAGENT => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:75.0) Gecko/20100101 Firefox/75.0',
+      CURLOPT_USERAGENT => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0',
     ];
     if (isset($referrer))
       $curl_opt[CURLOPT_REFERER] = $referrer;

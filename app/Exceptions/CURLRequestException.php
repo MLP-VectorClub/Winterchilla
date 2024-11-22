851fc98fb5192825bac9fb683267818842eb3ef4
SeinopSys <seinopsys@protonmail.com>
2024-11-22 21:16:16 +0100

    Log different info for 403 errors when loading oembed data

--- app/Exceptions/CURLRequestException.php
+++ app/Exceptions/CURLRequestException.php
@@ -5,7 +5,7 @@ namespace App\Exceptions;
 use Exception;
 
 class CURLRequestException extends Exception {
-  public function __construct($errMsg, $errCode, string $curlError) {
+  public function __construct($errMsg, $errCode, string $curlError, public readonly ?string $responseHeaders = null, public readonly ?string $response = null) {
     parent::__construct("$errMsg (HTTP $errCode)", $errCode);
     if (!empty($curlError))
       $this->message .= "; $curlError";

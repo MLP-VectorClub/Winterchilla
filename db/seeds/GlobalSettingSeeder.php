af2848a683f6d285d20b244eb296d5557c6f1ca5
WentTheFox <mail@went.tf>
2026-05-02 14:43:02 +0200

    Fix GlobalSettingSeeder

--- db/seeds/GlobalSettingSeeder.php
+++ db/seeds/GlobalSettingSeeder.php
@@ -6,12 +6,12 @@ class GlobalSettingSeeder extends AbstractSeed {
   public function run(): void {
     $data = [
       [
-        'key' => 'about_reservations',
-        'value' => '<p>People usually get excited whenever a new episode comes out, and start making vectors of any pose/object/etc. that they found hilarious/interesting enough. It often results in various people unnecessarily doing the very same thing. Vector Reservations can help organize our efforts by listing who\'s working on what and to reduce the number of duplicates.</p>',
+        'name' => 'about_reservations',
+        'val' => '<p>People usually get excited whenever a new episode comes out, and start making vectors of any pose/object/etc. that they found hilarious/interesting enough. It often results in various people unnecessarily doing the very same thing. Vector Reservations can help organize our efforts by listing who\'s working on what and to reduce the number of duplicates.</p>',
       ],
       [
-        'key' => 'reservation_rules',
-        'value' => '<ol><li>You MUST have an image to make a reservation! For the best quality, get your references from the episode in 1080p.</li>
+        'name' => 'reservation_rules',
+        'val' => '<ol><li>You MUST have an image to make a reservation! For the best quality, get your references from the episode in 1080p.</li>
 	<li>Making a reservation does NOT forbid other people from working on a pose anyway. It is only information that you are working on it, so other people can coordinate to avoid doing the same thing twice.</li>
 	<li>There are no time limits, but remember that the longer you wait, the greater the chance that someone might take your pose anyway. It\'s generally advised to finish your reservations before a new episode comes out.</li>
 	<li>The current limit for reservations is 4 at any given time. You can reserve more after completing or cancelling any previous reservations.</li>
@@ -19,8 +19,8 @@ class GlobalSettingSeeder extends AbstractSeed {
 </ol>',
       ],
       [
-        'key' => 'dev_role_label',
-        'value' => 'staff',
+        'name' => 'dev_role_label',
+        'val' => 'staff',
       ],
     ];
 
